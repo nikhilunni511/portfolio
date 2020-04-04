e21351edd1bb55ed83c0920c3fdb316b68d3d660
nikhilunni511 <nikhi.unni@gmail.com>
2020-04-04 23:34:47 +0530

    Updates

---
+++ precache-manifest.32072469d05fa14269e5c767b266a688.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "67f599392ba09c43fc3ae9c73fdb8068",
+    "url": "/portfolio/index.html"
+  },
+  {
+    "revision": "863509a8a14f8dea4d7c",
+    "url": "/portfolio/static/css/main.1c0d91fd.chunk.css"
+  },
+  {
+    "revision": "1e43d1afbccebb294b78",
+    "url": "/portfolio/static/js/2.4a32d523.chunk.js"
+  },
+  {
+    "revision": "e1ebcee444532ceab7d2a4a1c7007bba",
+    "url": "/portfolio/static/js/2.4a32d523.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "863509a8a14f8dea4d7c",
+    "url": "/portfolio/static/js/main.4cf00625.chunk.js"
+  },
+  {
+    "revision": "1e74db4740f99c68a0bf",
+    "url": "/portfolio/static/js/runtime-main.b37861ed.js"
+  }
+]);
